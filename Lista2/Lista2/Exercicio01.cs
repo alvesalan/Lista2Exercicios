465b012a2514a0a54eb35b5fa218efd9cb98b9f4
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-05-01 11:27:03 -0300

    Exercicio 1 refeito (pronto)
    Refazendo lista 2 e adicionando try nos exercicios

---
+++ Lista2/Lista2/Exercicio01.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+{
+    internal class Exercicio01
+    {
+        public Exercicio01()
+        {
+            string ReadLineValue = "";
+            int Numero = 0;
+
+               Console.Write("Diga um numero: ");
+
+            try
+            {
+
+                ReadLineValue = Console.ReadLine();
+                Numero = Convert.ToInt32(ReadLineValue);
+
+                
+
+            }
+            catch (Exception error)
+            {
+                
+                Console.WriteLine("Voce digitou ("+ ReadLineValue + ") digite apenas numero." + error);
+                Console.Write("Diga um numero: ");
+                Numero = Convert.ToInt32(Console.ReadLine());
+               
+
+            }
+
+            if (Numero == 0)
+            {
+                Console.WriteLine("Numero neutro");
+            }
+            else if (Numero > 0)
+            {
+                Console.WriteLine("Numero positivo");
+            }
+            else if(Numero < 0)
+            {
+                Console.WriteLine("Numero não e positivo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
