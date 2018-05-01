34835f955b0702c57985f24e98d2c24bacf9c8f3
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-05-01 20:24:08 -0300

    Exercicio 05 pronto
    ex pronto + try

---
+++ Lista2/Lista2/Exercicio05.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+    {
+        internal class Exercicio05
+        {
+            public Exercicio05()
+            {
+                int N = 0;
+
+                try
+                {
+                    Console.Write("Me diga um numero: ");
+                    N = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Digite apenas numero porfavor !!");
+                    Console.Write("Me diga um numero: ");
+                    N = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+                if (N == 0)
+                {
+                    Console.WriteLine("Numero neutro");
+                }
+                else if (N % 2 == 1)
+                {
+                    Console.WriteLine("O numero e negativo");
+                }
+                else if (N % 2 == 0)
+                {
+                    Console.WriteLine("O numero não e negativo");
+                }
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+
+    
